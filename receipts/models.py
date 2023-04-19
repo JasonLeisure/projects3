538846538577cc452496c3cc94d1e5152e74488a
Jason Leisure <jasonleisure1680@gmail.com>
2023-04-19 13:08:22 -0700

    Feature 10 Complete

--- receipts/models.py
+++ receipts/models.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.db import models
 
 
-# Create your models here.
 class ExpenseCategory(models.Model):
     name = models.CharField(max_length=50)
     owner = models.ForeignKey(
